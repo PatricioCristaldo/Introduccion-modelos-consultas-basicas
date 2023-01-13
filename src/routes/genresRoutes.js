6f33d153626153bff59107a8fb22d1d9b995e6c6
PatricioCristaldo <106325545+PatricioCristaldo@users.noreply.github.com>
2023-01-13 14:41:55 -0300

    finalizado tp19

--- src/routes/genresRoutes.js
+++ src/routes/genresRoutes.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const genresController = require('../controllers/genresController');
 
-//router.get('/genres', genresController.list);
-//router.get('/genres/detail/:id', genresController.detail);
-
+router.get('/genres', genresController.list);
+router.get('/genres/detail/:id', genresController.detail);
 
 module.exports = router;
